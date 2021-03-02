4a0324a5b707383d1a50c2cfcb4d94effdd3fdce
Violetta Pavlova <msviopavlova@gmail.com>
2021-03-02 14:00:30 +0000

    my first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 / num2;
+            float f1 = 3.5f;
+            Console.WriteLine(sumD);
+            Console.Read();
+        }
+    }
+}
